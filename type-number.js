d2b21c8aa130059771b9cccde1280fc930278aab
GermanEdu <germano.edu@gmail.com>
2021-09-05 12:07:58 -0300

    Aula um

---
+++ type-number.js
@@ -0,0 +1,53 @@
+// tipo number 
+
+// const { text } = require("stream/consumers");
+
+// const meuNumero = 5;
+
+// const num1 = 2;
+
+// const num2 = 2;
+
+// const operacao = num1 * num2;
+
+
+// console.log(operacao);
+// console.log(meuNumero);
+
+// POnto flutuante 
+
+// const pontos = 3.3;
+
+// const pontos2 = 1.5;
+
+// const multi = pontos / pontos2;
+
+// console.log(pontos);
+// console.log(pontos2);
+// console.log(multi);
+
+// const texto = "alura";
+
+// console.log(texto * texto);
+
+// const texto = "Ola mundo";
+
+// const textoDois = 'Ola mundo';
+
+// var senha = "123634Kaeyt7";
+
+// const citacao = 'O leo disse "Oi!"';
+
+// console.log(citacao);
+
+// // template string 
+
+// console.log(citacao + "sua senha é " + senha);
+
+// Boolean aula 
+
+const X = "Oi";
+const Y = "oi";
+
+console.log(X === Y);
+
